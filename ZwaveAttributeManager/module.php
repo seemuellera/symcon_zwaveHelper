77dcac5de720643704319aad0e41e5bd567f25d6
Andreas Seemueller <andreas.seemueller@gmail.com>
2022-04-06 21:52:31 +0200

    Added module for setting Z-Wave Parameter values

---
+++ ZwaveAttributeManager/module.php
@@ -0,0 +1,185 @@
+<?php
+
+// GUID to identify the Z-Wave devices
+define("ZWAVE_DEVICE_GUID","{101352E1-88C7-4F16-998B-E20D50779AF6}");
+
+// colors
+define("COLOR_OK","green");
+define("COLOR_WARN","orange");
+define("COLOR_CRIT","red");
+
+// Klassendefinition
+class ZwaveHelper extends IPSModule {
+ 
+	// Der Konstruktor des Moduls
+	// Überschreibt den Standard Kontruktor von IPS
+	public function __construct($InstanceID) {
+
+		// Diese Zeile nicht löschen
+        parent::__construct($InstanceID);
+ 
+        // Selbsterstellter Code
+    }
+ 
+    // Überschreibt die interne IPS_Create($id) Funktion
+    public function Create() {
+            
+		// Diese Zeile nicht löschen.
+		parent::Create();
+
+		// Properties
+		$this->RegisterPropertyString("Sender","ZwaveAttributeManager");
+		$this->RegisterPropertyInteger("RefreshInterval",0);
+		$this->RegisterPropertyBoolean("DebugOutput",false);
+		$this->RegisterPropertyInteger("ZwaveInstanceId",0);
+		$this->RegisterPropertyInteger("ParameterId",0);
+		
+		// Variables
+		$this->RegisterVariableInteger("TargetValue", "Target Value");
+		$this->RegisterVariableInteger("CurrentValue", "Current Value");
+		$this->RegisterVariableBoolean("ParameterInSync","Parameter Value in Sync","~Alert");
+		$this->RegisterVariableBoolean("Status","Status","~Switch");
+		
+		// Default Actions
+		$this->EnableAction("TargetValue");
+		$this->EnableAction("Status");
+
+		// Timer
+		$this->RegisterTimer("RefreshInformation", 0 , 'ZWAM_RefreshInformation($_IPS[\'TARGET\']);');
+    }
+
+	public function Destroy() {
+
+		// Never delete this line
+		parent::Destroy();
+	}
+ 
+    // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+    public function ApplyChanges() {
+
+		
+		$newInterval = $this->ReadPropertyInteger("RefreshInterval") * 1000;
+		$this->SetTimerInterval("RefreshInformation", $newInterval);
+		
+       	// Diese Zeile nicht löschen
+       	parent::ApplyChanges();
+    }
+
+
+	public function GetConfigurationForm() {
+
+        	
+		// Initialize the form
+		$form = Array(
+            	"elements" => Array(),
+				"actions" => Array()
+        	);
+
+		// Add the Elements
+		$form['elements'][] = Array("type" => "NumberSpinner", "name" => "RefreshInterval", "caption" => "Refresh Interval");
+		$form['elements'][] = Array("type" => "CheckBox", "name" => "DebugOutput", "caption" => "Enable Debug Output");
+		$form['elements'][] = Array("type" => "SelectInstance", "name" => "ZwaveInstanceId", "caption" => "Zwave Instance", "validModules" => Array(ZWAVE_DEVICE_GUID));
+		$form['elements'][] = Array("type" => "NumberSpinner", "name" => "ParameterId", "caption" => "Parameter ID");
+		
+		// Add the buttons for the test center
+		$form['actions'][] = Array("type" => "Button", "label" => "Refresh Overall Status", "onClick" => 'ZWAM_RefreshInformation($id);');
+		
+		// Return the completed form
+		return json_encode($form);
+
+	}
+
+	public function RefreshInformation() {
+
+		if (! GetValue($this->GetIDForIdent("Status")) ) {
+
+			$this->LogMessage("Skipping Refresh because instance Status is off", "DEBUG");
+			return;
+		}
+
+		$this->LogMessage("Refresh in progress","DEBUG");
+		
+		$this->GetCurrentValue();
+
+		if ( ReadValue($this->GetIDForIdent("CurrentValue")) == ReadValue($this->GetIDForIdent("TargetValue")) ) {
+
+			SetValue($this->GetIDForIdent("ParameterInSync"), true);
+		}
+		else {
+
+			SetValue($this->GetIDForIdent("ParameterInSync"), false);
+		}
+	}
+	
+	public function RequestAction($Ident, $Value) {
+	
+		switch ($Ident) {
+				
+			case "Status":
+				SetValue($this->GetIDForIdent($Ident), $Value);
+				break;
+			case "TargetValue":
+				$this->SetTargetValue($Value);
+				IPS_Sleep(500);
+				$this->RefreshInformation();
+				break;
+			default:
+				throw new Exception("Invalid Ident");
+			
+		}
+	}
+		
+	protected function LogMessage($message, $severity = 'INFO') {
+		
+		$logMappings = Array();
+		// $logMappings['DEBUG'] 	= 10206; Deactivated the normal debug, because it is not active
+		$logMappings['DEBUG'] 	= 10201;
+		$logMappings['INFO']	= 10201;
+		$logMappings['NOTIFY']	= 10203;
+		$logMappings['WARN'] 	= 10204;
+		$logMappings['CRIT']	= 10205;
+		
+		if ( ($severity == 'DEBUG') && ($this->ReadPropertyBoolean('DebugOutput') == false )) {
+			
+			return;
+		}
+		
+		$messageComplete = $severity . " - " . $message;
+		parent::LogMessage($messageComplete, $logMappings[$severity]);
+	}
+	
+	public function GetCurrentValue() {
+
+		if (! GetValue($this->GetIDForIdent("Status")) ) {
+
+			$this->LogMessage("Skipping get current value because instance Status is off", "DEBUG");
+			return;
+		}
+
+		$result = ZW_GetConfigurationValue($this->ReadPropertyInteger("ZwaveInstanceId"), $this->ReadPropertyInteger("ParameterId"));
+
+		if ($result) {
+
+			SetValue($this->GetIDForIdent("CurrentValue"), $result);
+		}
+	}
+
+	public function SetTargetValue($newValue) {
+
+		if (! GetValue($this->GetIDForIdent("Status")) ) {
+
+			$this->LogMessage("Skipping set value because instance Status is off", "DEBUG");
+			return;
+		}
+
+		SetValue($this->GetIDForIdent("TargetValue"), $newValue);
+
+		$result = ZW_SetConfigurationValue($this->ReadPropertyInteger("ZwaveInstanceId"), $this->ReadPropertyInteger("ParameterId"), $newValue);
+
+		if (! $result) {
+
+			$this->LogMessage("Unable to set configuration value", "CRIT");
+		}
+	}
+}
+?>
